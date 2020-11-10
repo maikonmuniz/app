fb6de89a52d80273a891fa0767c8bbc9b71873b4
lhsfranca <franca.luiz4@hotmail.com>
2020-11-10 20:09:59 -0300

    AC05 - version 1

--- app/src/main/java/com/example/ctre/LMSDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ctre
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = arrayOf(Forum::class), version = 1)
-abstract class LMSDatabase: RoomDatabase() {
-    abstract fun forumDAO(): ForumDAO
-}
